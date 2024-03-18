30fbf684f89bf10b1cfff781d4e0f521687d00d5
Francis-JSM <163810795+Francis-JSM@users.noreply.github.com>
2024-03-18 10:33:25 +0530

    Update settings.py

--- startup/settings.py
+++ startup/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,6 +129,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/var/www/static/',
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Media Files
